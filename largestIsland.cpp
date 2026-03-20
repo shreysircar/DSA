70363bd8c805bbaeb33d22fb4253d215adaf0ceb
shreysircar <sircarshrey@gmail.com>
2026-03-20 08:56:47 +0530

    LC  827

--- largestIsland.cpp
+++ largestIsland.cpp
@@ -1,3 +1,12 @@
+/*         Scroll below to see JAVA code as well        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=iCAC-QrQ-4A
+    Company Tags                : Google, Uber
+    Leetcode Link               : https://leetcode.com/problems/making-a-large-island/
+*/
+
+
+/********************************************************************** C++ **********************************************************************/
 //Approach - 1 - Brute Force DFS (you can do BFS as well)
 //T.C : O(n^4)
 //S.C : O(n^2)
@@ -52,5 +61,235 @@ public:
 
     int largestIsland(vector<vector<int>>& grid) {
         return findLargestIsland(grid);
+    }
+};
+
+//Approach - 2 - Better Brute Force DFS (you can do BFS as well)
+//T.C : O(n^4)
+//S.C : O(n^2)
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    int DFS(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>& visited) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 || visited[i][j])
+            return 0;
+
+        visited[i][j] = true;
+        int count = 1;
+
+        for(vector<int>& dir : directions) {
+            count += DFS(grid, i + dir[0], j + dir[1], visited);
+        }
+
+        return count;
+    }
+
+    int findLargestIsland(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int maxArea = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 1 && !visited[i][j]) {
+                    int size = DFS(grid, i, j, visited);
+                    maxArea = max(maxArea, size);
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) {  // Try converting each 0 to 1
+                    grid[i][j] = 1;
+                    
+                    visited = vector<vector<bool>>(n, vector<bool>(n, false));
+                    int size = DFS(grid, i, j, visited);
+                    
+                    maxArea = max(maxArea, size);
+                    grid[i][j] = 0; // Backtrack
+                }
+            }
+        }
+        
+        return (maxArea == 0) ? m * n : maxArea; // If the grid was full of 1s
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        return findLargestIsland(grid);
+    }
+};
+
+
+//Approach - 3  - Optimal DFS (You can use BFS as well)
+//T.C : O(m*n)
+//S.C : O(m*n)
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> directions{{1, 0}, {-1, 0},{0, 1},{0, -1}};
+
+    int DFS(vector<vector<int>>& grid, int i, int j, int& id) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1)
+            return 0;
+        
+        grid[i][j] = id;
+        int count  = 1;
+        
+        for(vector<int>& dir : directions) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            
+            count += DFS(grid, x, y, id);
+        }
+        
+        return count;
+    }
+    int largestIsland(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int maxArea = 0;
+        unordered_map<int, int> mp;
+        int island_id = 2;
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    int size = DFS(grid, i, j, island_id);
+                    maxArea = max(maxArea, size);
+                    mp[island_id] = size;
+                    island_id++;
+                }
+            }
+        }
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 0) {
+                    unordered_set<int> st;
+                    for(vector<int>& dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != 0)
+                            st.insert(grid[x][y]);
+                    }
+                    
+                    int sum = 1; //converting current 0 to 1
+                    for(const int &s : st) {
+                        sum += mp[s];
+                    }
+                    maxArea = max(maxArea, sum);
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+};
+
+
+//Approach-4 (Using DSU)
+//T.C : O(m*n * alpha(m*n))
+//S.C : O(m*n)
+class DSU {
+    vector<int> parent;
+    vector<int> size;
+
+public:
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]); // Path Compression
+    }
+
+    void Union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        if (size[rootX] > size[rootY]) {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        } else {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        }
+    }
+
+    int getSize(int x) {
+        return size[find(x)];
+    }
+};
+
+class Solution {
+public:
+    // Global directions array
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int totalCells = n * n;
+        DSU dsu(totalCells);
+        int maxArea = 0;
+
+        // Step 1: Connect all existing 1s using DSU
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int idx = i * n + j;
+                    for (auto& dir : directions) {
+                        int i_ = i + dir[0];
+                        int j_ = j + dir[1];
+                        int idx_ = i_ * n + j_;
+                        if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < n && grid[i_][j_] == 1) {
+                            dsu.Union(idx, idx_);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Step 2: Find the largest island without modification
+        for (int idx = 0; idx < totalCells; idx++) {
+            if (grid[idx / n][idx % n] == 1) {
+                maxArea = max(maxArea, dsu.getSize(idx));
+            }
+        }
+
+        // Step 3: Try flipping each 0 to 1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    unordered_set<int> uniqueParents;
+                    for (auto& dir : directions) {
+                        int i_   = i + dir[0];
+                        int j_   = j + dir[1];
+                        int idx_ = i_ * n + j_;
+                        if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < n && grid[i_][j_] == 1) {
+                            uniqueParents.insert(dsu.find(idx_));
+                        }
+                    }
+
+                    int newSize = 1; // Flip this 0 to 1
+                    for (int parent : uniqueParents) {
+                        newSize += dsu.getSize(parent);
+                    }
+                    maxArea = max(maxArea, newSize);
+                }
+            }
+        }
+
+        return maxArea;
     }
 };
