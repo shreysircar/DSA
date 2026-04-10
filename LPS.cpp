de0b47f18f4ef0700c39943aaf19ddfa096a06df
shreysircar <sircarshrey@gmail.com>
2026-04-10 18:28:31 +0530

    LC 516

---
+++ LPS.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+int LCS(string text1, string text2){
+    int n = text1.size();
+    int m = text2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i = n-1; i >= 0; i--){
+        for(int j = m-1; j >= 0; j--){
+            if(text1[i] == text2[j]){
+                dp[i][j] = 1 + dp[i+1][j+1];
+            } else {
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+    }
+
+    return dp[0][0];
+}
+
+    int longestPalindromeSubseq(string s) {
+        string s2;
+        for(int i=s.size()-1;i>=0;i--){
+s2.push_back(s[i]);
+        }
+        return LCS(s,s2);
+    }
+};
